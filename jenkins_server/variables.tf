8b76147ac389e0aafc99bcba2e14e70bcff5940a
R7Murat <arseven.murat@gmail.com>
2025-09-05 00:37:43 +0300

    m

---
+++ jenkins_server/variables.tf
@@ -0,0 +1,17 @@
+variable "mykey" {
+  default = "arseven" 
+}
+
+variable "instancetype" {
+  default = "t3a.medium"
+}
+variable "tag" {
+  default = "Jenkins_Server_techpro"
+}
+variable "jenkins-sg" {
+  default = "jenkins-server-sec-gr"
+}
+
+variable "user" {
+  default = "arseven"
+}
